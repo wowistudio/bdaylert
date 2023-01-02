d098715651d240775be9d83d794be2eca1532d53
Jeroen Huisman <jeroen.huisman@biller.ai>
2023-01-02 13:20:58 +0100

    rotate keys. put secrets in .env

--- local/get_gcal_creds.sh
+++ local/get_gcal_creds.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+envfile=$(dirname $(realpath $0))/.env
+export $(cat $envfile | xargs) > /dev/null
 export PYTHONPATH=$(dirname $(realpath $0))/../src
+
 python -c 'from bday.gcal.authorize import authorize; authorize()'
--- src/bday/settings.py
+++ src/bday/settings.py
@@ -1,22 +1,27 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-TELEGRAM_BOT_TOKEN = "5833793075:AAElkx4ei48BA030Jzl_PhKUF6Y72zJkxWQ"
-TELEGRAM_CHAT_ID = "-828478140"
+# when local development you can store your secret envs in <project-root>/local/.env
+if os.path.exists(os.environ.get("DOTENV_FILE", "")):
+    load_dotenv(Path(os.environ.get("DOTENV_FILE")))
+
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 TELEGRAM_BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
-TELEGRAM_ALLOWED_USERS = [5972175655]
+TELEGRAM_ALLOWED_USERS = os.environ["TELEGRAM_ALLOWED_USERS"].split(",")
 
 GCAL_SCOPES = ["https://www.googleapis.com/auth/calendar"]
 GCAL_CAL_ID_PREFIX = "18c770069a5f2babc767018f12cb0ce1f6df9bc3c785a5ce7b468966ceb7ca4c"
 GCAL_CAL_ID = f"{GCAL_CAL_ID_PREFIX}@group.calendar.google.com"
-GCAL_CLIENT_SECRET = "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"
 GCAL_OAUTH_CREDS = {
     "installed": {
-        "client_id": "505808692622-asdoj2r0mkdp50plbaajclrie2ct55mq.apps.googleusercontent.com",
+        "client_id": os.environ["GCAL_CLIENT_ID"],
         "project_id": "bdaylert",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": GCAL_CLIENT_SECRET,
+        "client_secret": os.environ["GCAL_CLIENT_SECRET"],
         "redirect_uris": ["http://localhost"],
     }
 }
